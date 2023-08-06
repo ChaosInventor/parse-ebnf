4c97d600f0d265374360efaf767f619db439ddb0
ChaosInventor <chaosinventor@yandex.com>
2023-08-06 15:22:21 +0200

    Add useable classes docs

--- docs/classes.rst
+++ docs/classes.rst
@@ -1,5 +1,16 @@
 Classes
 =======
 
-Stub
+To use this package, you'll only really need two classes:
+
+- |AST|
+- |ASTNode|
+
+Use |AST| to parse your input, and |ASTNode| and it's
+:ref:`derived classes <todo>` to traverse the tree.
+
+.. autoclass:: parse_ebnf.AST
+   :members: parse, unparse, write
+
+.. autoclass:: parse_ebnf.ASTNode
 
--- parse_ebnf/__init__.py
+++ parse_ebnf/__init__.py
@@ -3,6 +3,29 @@
 # SPDX-License-Identifier: MIT
 
 class ParserState:
+    """A helper class for parsing, not meant to be used externally.
+
+    Keeps track of the current line and column, `line` and `column`
+    respectively, as well as the currently read string, `c`. It also has a
+    reference to the AST it belongs, `ast`, and to the read function `readFunc`
+
+    Has lists containing constant characters that are used as terminals those
+    being:
+
+     - `DEFINITION_SEPARATORS`
+     - `PRODUCT_TERMINATOR_SYMBOLS`
+     - `TERMINAL_START_SYMBOLS`
+     - `TERM_START_SYMBOLS`
+     - `BRACKET_START_SYMBOLS`
+     - `BRACKET_END_SYMBOLS`
+
+    Finally has two helper functions for reading input:
+
+     - `read`
+     - `readNoEOF`
+
+    They help by maintaining `line`, `column` and `c`.
+    """
     ast = None
     readFunc = None
     c = ''
@@ -17,6 +40,7 @@ class ParserState:
     BRACKET_END_SYMBOLS = [')', '/)', ':)']
 
     def read(self, n):
+        """Reads `n` characters, updates `line`, `column` and `c`, returns self.c"""
         self.c = self.readFunc(n)
         for c in self.c:
             if c == '\n':
@@ -27,13 +51,20 @@ class ParserState:
 
         return self.c
     def readNoEOF(self, n, msg="Unexpected EOF"):
+        """Just like `read` except that it raises a `SyntaxError` with `msg` if
+        no characters were read, that is EOF has been reached.
+        """
         self.read(n)
+
         if len(self.c) == 0:
             raise SyntaxError(msg)
 
         return self.c
 
     def __init__(self, read, ast):
+        """ Create a new parser state for the AST `ast` with the read function
+        `read`.
+        """
         assert isinstance(ast, AST), "Expected an AST obejct"
         assert read != None, "Expected a read function"
         self.ast = ast
@@ -44,16 +75,81 @@ class ParserState:
 
 
 class AST:
+    """A parse tree for EBNF.
+
+    Contains the following variables:
+
+    - ``root``, the root node of the tree, an instance of :py:class:`ASTRootNode`;
+    - ``count``, the number of nodes in the tree;
+    - ``height``, the height of the tree;
+    - ``maxDegree``, the maximum number of children that a single node has in
+      the tree.
+
+    And the following functions:
+
+    - ``parse``, use with a read function(a function that works just like the
+      read function for files) to parse an input.
+    - ``unparse``, use with a write function(a function that works just like the
+      write function for files) to write the text that the parse tree was
+      created from.
+    - ``write``, use with a write function to dump a textual representation of
+      the parse tree, mainly meant for debugging.
+
+    Example:
+
+    .. code-block:: python
+
+        from parse_ebnf import AST
+        from io import StringIO
+
+        ast1 = AST()
+        ast2 = AST()
+
+        file = open('your-ebnf-file.ebnf', 'r')
+
+        ast1.parse(file.read);
+        with StringIO('rule = term | another term;') as f:
+            ast2.parse(f.read)
+
+        #You now have two useable parse trees, ast1 and ast2
+
+        #Print the text that the first child of the root was created from, the
+        #first child will probably be an ASTCommentNode or ASTRuleNode.
+        print(repr(ast.root.children[0]))
+
+        #The height and maxDegree can be used to calculate the worst case size
+        #for:
+        #
+        # - A stack, the worst case size would be the height;
+        # - A queue, the worst case size would be `maxDegree ** height`.
+
+        file.close()
+    """
     root = None
     count = 0
     height = 0
     maxDegree = 0
 
     def parse(self, read):
+        """Parse an input from ``read``.
+
+        ``read`` is a function that works exactly like read for files - it takes
+        in a positive number or zero, and returns a string of that size or less
+        if EOF is near.
+        """
         self.root.parse(ParserState(read, self))
     def unparse(self, write):
+        """Write the text this tree was generate to using ``write``.
+
+        ``write`` is a function that works exactly like write for files - it
+        takes in a string and write it to the output. Note that the return value
+        in this case is ignored.
+        """
         write(repr(self.root))
     def write(self, write):
+        """Same as `unparse` except that a textual representation meant for
+        debugging is written.
+        """
         write(str(self))
 
     def __init__(self):
@@ -62,12 +158,52 @@ class AST:
         self.height = 0
         self.maxDegree = 0
     def __repr__(self):
+        """Returns the text that this tree was generated from."""
         return repr(self.root)
     def __str__(self):
+        """Returns a textual representation of this tree meant for debugging."""
         return f'AST{{count = {self.count}, height = {self.height}, maxDegree={self.maxDegree}}}:\n{str(self.root)}'
 
 
 class ASTNode:
+    """Base class of all AST nodes.
+
+    AST nodes differ only in what nodes are their ``parent`` and which nodes
+    are their ``children``, more info at the :doc:`tree structure <tree>`.
+    This node type in particular is not used in the |AST|, it only serves as a
+    base class for all other node types. Using ``isinstance`` you can resolve
+    a node's type.
+
+    Contains the following variables:
+
+    - ``parent``, the parent node of this node. It is ``None`` only for
+      |ASTRootNode|.
+    - ``children``, a list of this node's children. Empty only for instance of
+      |ASTTextNode|.
+    - ``depth``, an integer denoting how deep a node is in the tree. The root is
+      defined as being at depth 0, it's children at depth 1, etc.
+    - ``startLine``, the line where the text that this node is comprised of
+      starts, inclusively. Counting starts from 1, and is incremented each time
+      a newline is encountered in the input;
+    - ``startColumn``, the column where the text that this node is comprised of
+      starts, inclusively. Counting starts at 1, and in incremented every
+      character. Each newline resets the counter to 0;
+    - ``endLine``, like ``startLine`` except that this is where the text ends,
+      exclusively;
+    - ``endColumn``, like ``startColumn`` except that this is where the text ends,
+      exclusively.
+
+    The following always holds for coordinates:
+
+    - `startLine` >= 0
+    - `endLine` >= 0
+    - `startColumn` >= 0
+    - `startLine` <= `endLine`
+    - if `startColumn` > `endColumn`: repr(node) == '' else `endColumn` >= 0
+
+    .. note :: The end coordinates also take into account child nodes. The
+       children's text is also counted as the parent's text.
+    """
     parent = None
     children = []
     startLine = 0
