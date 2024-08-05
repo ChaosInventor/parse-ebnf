f57f7d5397a21e95b3c8d9a2144aa373645c9a79
ChaosInventor <chaosinventor@yandex.com>
2024-08-05 20:16:18 +0200

    Document child spec functions

--- tests/tree_structure/__init__.py
+++ tests/tree_structure/__init__.py
@@ -20,6 +20,10 @@ def clean_args(*args):
         else: ret.append(arg)
     return ret
 def literal(s):
+    """A singe |Literal| instance is expected.
+
+    The data that the |Literal| holds is either one of the arguments.
+    """
     def l(node, index):
         assert index < len(node.children), "Ran out of children nodes for literal"
 
@@ -45,6 +49,11 @@ def predicate(*args):
 
     return p
 def either(*args):
+    """Only one of the arguments are expected.
+
+    The arguments may be node types or other functions. In any case, only one of
+    them may follow in the child list.
+    """
     cleaned = clean_args(*args)
 
     def e(node, index):
@@ -56,6 +65,20 @@ def either(*args):
 
     return e
 def zero_or_more(*args):
+    """Any finite number or zero of the arguments are expected.
+
+    Any number of node types and functions may be specified. In all cases, they
+    are expected to follow in the child list in the order they are specified.
+
+    For example:
+
+    .. code-block:: py
+
+       zero_or_more(either(Comment, Product), Space)
+
+    Means that either a |Comment| or |Product| followed by a |Space| are
+    expected to occur any number of times in the child list.
+    """
     cleaned = clean_args(*args)
 
     def a(node, index):
@@ -72,6 +95,22 @@ def zero_or_more(*args):
 
     return a
 def maybe(*args):
+    """The arguments may or may not occur.
+
+    The arguments may be node types or other functions. In all cases, either all
+    of the arguments occur in the child list in the specified order or they do
+    not occur at all.
+
+    For example:
+
+    .. code-block:: py
+
+       maybe(Space, zero_or_more(Term, Product), Space)
+
+    Means that a single sequence of a |Space| followed by any number of |Term|,
+    |Product| pairs followed by a single |Space| may occur in the child list, or
+    not at all.
+    """
     def m(node, index):
         if index >= len(node.children): return index
         p = predicate(*args)
